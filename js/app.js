33f3345b61164903a747f67580c571f088fa1536
Faraz <af.ahmfrz@gmail.com>
2017-06-10 14:04:44 +0530

    Add multiple cats

--- js/app.js
+++ js/app.js
@@ -1,8 +1,35 @@
-var Cat = function(){
+var initialCats = [{
+    clickCount: 0,
+    name: 'Tabby',
+    imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+    nicknames: ["Tabs", "Cutie", "OceanEye"]
+}, {
+    clickCount: 0,
+    name: 'Tiger',
+    imgSrc: 'img/1413379559_412a540d29_z.jpg',
+    nicknames: ["Lion heart"]
+}, {
+    clickCount: 0,
+    name: 'Scaredy',
+    imgSrc: 'img/4154543904_6e2428c421_z.jpg',
+    nicknames: ["Scary cute"]
+}, {
+    clickCount: 0,
+    name: 'Shadow',
+    imgSrc: 'img/9648464288_2516b35537_z.jpg',
+    nicknames: ["Ninja Cat"]
+}, {
+    clickCount: 0,
+    name: 'Sleepy',
+    imgSrc: 'img/22252709_010df3379e_z.jpg',
+    nicknames: ["Sleep Dog"]
+}]
+
+var Cat = function(data) {
     // Observables
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
 
     // Computed Observables
     this.level = ko.computed(function() {
@@ -17,14 +44,27 @@ var Cat = function(){
     }, this);
 
     // Observable array
-    this.nicknames = ko.observableArray(["Tabs", "Cutie", "OceanEye"]);
+    this.nicknames = ko.observableArray(data.nicknames);
 };
 var ViewModel = function() {
-    this.currentCat = ko.observable(new Cat());
+    // To handle changing binding context
+    // Self always maps to the view model, regardless of current context
+    var self = this;
+
+    this.catList = ko.observableArray([]);
+    initialCats.forEach(function(cat){
+        self.catList.push(new Cat(cat));
+    });
+
+    this.currentCat = ko.observable(this.catList()[0]);
 
     // Observable
     this.incrementCounter = function() {
-        this.clickCount(this.clickCount() + 1);
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+    }
+
+    this.updateCat = function(cat){
+        self.currentCat(cat);
     }
 }
 
